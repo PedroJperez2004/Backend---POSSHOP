183dbb5c05be9321377663ce96f6b26ee78c4896
PedroJperez2004 <perezpedrojose2018@gmail.com>
2026-02-03 13:19:01 -0500

    ss

--- src/config/auth_config.js
+++ src/config/auth_config.js
@@ -4,6 +4,4 @@ export const {
     REFRESH_JWT_SECRET
 } = process.env;
 
-if (!SECRET_JWT_KEY || !REFRESH_JWT_SECRET || !SALT_ROUNDS) {
-    throw new Error('❌ Variables de entorno de auth no configuradas');
-}
+
